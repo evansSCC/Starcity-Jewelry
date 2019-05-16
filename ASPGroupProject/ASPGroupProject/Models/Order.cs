98422928b116ec49cbb9a1307d41d62a8970ea57
Christopher N Perault <cp652540@southeast.edu>
2019-05-16 16:12:40 -0500

    added object classes

---
+++ ASPGroupProject/ASPGroupProject/Models/Order.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASPGroupProject.Models
+{
+    public class Order
+    {
+        public int ProductID { get; set; }
+        public int CustomerID { get; set; }
+        public DateTime OrderDate { get; set; }
+        public decimal OrderPrice { get; set; }
+        public bool ShippingAddressDifferentFromBilling { get; set; }
+
+        public Order(int product_ID, int customer_ID, DateTime orderDate, decimal priceOfOrder, bool isShippingDifferentThanBilling)
+        {
+            ProductID = product_ID;
+            CustomerID = customer_ID;
+            OrderDate = orderDate;
+            OrderPrice = priceOfOrder;
+            ShippingAddressDifferentFromBilling = isShippingDifferentThanBilling;
+        }
+    }
+}
