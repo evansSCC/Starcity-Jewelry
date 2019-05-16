c0d837c8e7bf2044e76df12185448010ddea1926
Evan <es555691@southeast.edu>
2019-05-16 11:50:38 -0500

    Default MVC file structure created for project

---
+++ ASPGroupProject/ASPGroupProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASPGroupProject.MvcApplication" Language="C#" %>
---
+++ ASPGroupProject/ASPGroupProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASPGroupProject.Startup))]
+namespace ASPGroupProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
