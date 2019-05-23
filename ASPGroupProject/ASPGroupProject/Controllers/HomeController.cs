489221d469912e971ac9a3ba3ca795e4cbda1b19
Christopher N Perault <cp652540@southeast.edu>
2019-05-23 16:03:00 -0500

    logic/data retrieval code does not go in a View. All data retrieval/instantiation must go in the Controller. Added test list for content iteration on Store page. Images are huge due to .raw format; CSS needs to be added for image size manipulation.

--- ASPGroupProject/ASPGroupProject/Controllers/HomeController.cs
+++ ASPGroupProject/ASPGroupProject/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ASPGroupProject.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -15,6 +16,15 @@ namespace ASPGroupProject.Controllers
 
         public ActionResult Store()
         {
+            List<Product> listOfProducts = new List<Product>();
+
+            listOfProducts.Add(new Product("test name 1", "sample description 1", "sample category 1", 35.00m, "..\\Images\\Products\\B114\\B114_Alt1.jpg", 3));
+            listOfProducts.Add(new Product("test name 2", "sample description 2", "sample category 2", 55.00m, "..\\Images\\Products\\B130\\B130_Alt.jpg", 5));
+            listOfProducts.Add(new Product("test name 3", "sample description 3", "sample category 3", 15.00m, "..\\Images\\Products\\N106\\N106_Main.jpg", 18));
+            listOfProducts.Add(new Product("test name 4", "sample description 4", "sample category 4", 60.00m, "..\\Images\\Products\\N147\\N147_Alt.jpg", 7));
+            listOfProducts.Add(new Product("test name 5", "sample description 5", "sample category 5", 10.00m, "..\\Images\\Products\\S001\\S001_Alt1.jpg", 2));
+
+            ViewBag.Message = listOfProducts;
             return View();
         }
         public ActionResult Shopping_Cart()
