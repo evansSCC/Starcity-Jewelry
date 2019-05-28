d90e1db94c61ff13810ebe2f8a035624f0adf28d
Christopher N Perault <cp652540@southeast.edu>
2019-05-28 15:51:07 -0500

    fixed view/controller code for Store to pull data from DB.

--- ASPGroupProject/ASPGroupProject/Controllers/HomeController.cs
+++ ASPGroupProject/ASPGroupProject/Controllers/HomeController.cs
@@ -16,13 +16,7 @@ namespace ASPGroupProject.Controllers
 
         public ActionResult Store()
         {
-            List<Product> listOfProducts = new List<Product>();
-
-            listOfProducts.Add(new Product("test name 1", "sample description 1", "sample category 1", 35.00m, "..\\Images\\Products\\B114\\B114_Alt1.jpg", 3));
-            listOfProducts.Add(new Product("test name 2", "sample description 2", "sample category 2", 55.00m, "..\\Images\\Products\\B130\\B130_Alt.jpg", 5));
-            listOfProducts.Add(new Product("test name 3", "sample description 3", "sample category 3", 15.00m, "..\\Images\\Products\\N106\\N106_Main.jpg", 18));
-            listOfProducts.Add(new Product("test name 4", "sample description 4", "sample category 4", 60.00m, "..\\Images\\Products\\N147\\N147_Alt.jpg", 7));
-            listOfProducts.Add(new Product("test name 5", "sample description 5", "sample category 5", 10.00m, "..\\Images\\Products\\S001\\S001_Alt1.jpg", 2));
+            List<Product> listOfProducts = ProductDA.GetAllProducts();
 
             ViewBag.Message = listOfProducts;
             return View();
